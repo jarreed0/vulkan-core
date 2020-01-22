c68fcf5f5d39377b15901a6b88c793eb0433dbd9
jarreed0 <jarreed0@gmail.com>
2020-01-22 12:29:17 -0700

    added doc notes

--- src/core/application.h
+++ src/core/application.h
@@ -10,30 +10,49 @@
 
 #include "log.h"
 
-#define VC_WINDOW GLFWwindow
+#define VC_WINDOW GLFWwindow //!<VCORE WINDOW MACRO
 
-namespace vCore { class Application {
+//!Namespace Used By Wrapper
+namespace vCore {
+//! Standar vCore Class To Create Application
+class Application {
 private:
-	std::string title;
-	int width, height;
-	VC_WINDOW* window;
-	bool open;
+	std::string title; //!<Window Title
+	int width, height; //!<Window size
+	VC_WINDOW* window; //!<Window Object
+	bool open; //!<Boolean if the window is open
 public:
+	//!Application Constructor
 	Application();
+	//!Application Deconstructor
 	~Application();
+	//!Create Window with default title and size
 	VC_WINDOW* createWindow();
+	//!Create Window with defualt size and specified title
 	VC_WINDOW* createWindow(std::string s);
+	//!Create Window with defualt title and specified size
 	VC_WINDOW* createWindow(int w, int h);
+	//!Create Window with specified title and size
 	VC_WINDOW* createWindow(std::string s, int w, int h);
+	//!Set Window Title
 	void setTitle(std::string t) {title=t;}
+	//!Get Window Width
 	int getWidth() {return width;}
+	//!Set Window Width
 	void setWidth(int w) {width=w;}
+	//!Get Window Height
 	int getHieght() {return height;}
+	//!Set Window Height
 	void setHeight(int h) {height=h;}
+	//!Set Window Size
 	void setSize(int w, int h) {setHeight(h);setWidth(w);}
+	//!Destroy Objects
 	void destroy();
+	//!Close Window
 	void close() {open=0;destroy();}
+	//!Update Window
 	bool update();
+	//!Get VC_WINDOW
 	VC_WINDOW* getWindow() {return window;}
 }; }
 
--- src/core/log.h
+++ src/core/log.h
@@ -27,36 +27,43 @@ https://stackoverflow.com/questions/2616906/how-do-i-output-coloured-text-to-a-l
 
 namespace vCore { //class logger {
 
+//! PRINT TO SCREEN
 template<typename T>
 void log(T a) {
  std::cout << a << "\n";
 }
 
+//! Display Error In Red
 template<typename T>
 void error(T a) {
  std::cout << "\033[1;31m" << a << "\033[0m\n";
 }
 
+//! Display Debug Message In Blue
 template<typename T>
 void debug(T a) {
  std::cout << "\033[1;34m" << a << "\033[0m\n";
 }
 
+//! Display Warning Highlighted Yellow
 template<typename T>
 void warning(T a) {
  std::cout << "\033[1;43m" << a << "\033[0m\n";
 }
 
+//! Display Success In Green
 template<typename T>
 void success(T a) {
  std::cout << "\033[1;32m" << a << "\033[0m\n";
 }
 
+//! Display Notice In White
 template<typename T>
 void notice(T a) {
  std::cout << "\033[1;37m" << a << "\033[0m\n";
 }
 
+//! Display Log With Set Color
 template<typename T>
 void log(T a, int x) {
  std::cout << "\033[1;" << x<< "m" << a << "\033[0m\n";
